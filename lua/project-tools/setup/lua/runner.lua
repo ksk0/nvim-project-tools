be53160714bf8b3c5a9840583de6298a1e5faa31
koske <ksk0@users.noreplay.github.com>
2023-02-10 12:52:58 +0100

    Added "init" option in "runner" section for "lua" project

--- lua/project-tools/setup/lua/runner.lua
+++ lua/project-tools/setup/lua/runner.lua
@@ -1,19 +1,13 @@
 -- setup/lua/runner
 --
--- local list   = require("project-tools.core.list")
--- local ppath = require("plenary.path")
 local scanner = require("plenary.scandir").scan_dir
-local env   = require("project-tools.core.env")
+local env = require("project-tools.core.env")
 
 local setup = function(_, pconfig)
   local tools = pconfig._config.tool
   if not tools then return end
 
-  -- print(vim.inspect(pconfig))
-
   local runner  = tools.runner or {}
-
-  -- print("Runner:" .. vim.inspect(runner))
 
   if runner.lib then
     local plib = pconfig._root .. '/' .. runner.lib
@@ -38,7 +32,16 @@ local setup = function(_, pconfig)
     for _,script in pairs(scripts) do
       vim.cmd('source ' .. script)
     end
-    
+  end
+
+  if runner.init then
+    local pinit = pconfig._root .. '/' .. runner.init
+
+    local ok,_ = pcall(dofile, pinit)
+    if not ok then
+      local msg = string.format('Init file: "%s" not found!', runner.init)
+      vim.notify(msg, "error")
+    end
   end
 end
 
