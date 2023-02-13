a9973796d27d8aece44fee1f73ea5cb9c82ebac7
koske <ksk0@users.noreplay.github.com>
2023-02-13 10:08:26 +0100

    Load language specific scripts

--- lua/project-tools.lua
+++ lua/project-tools.lua
@@ -7,12 +7,22 @@ M.test = function ()
 end
 
 return setmetatable(M, {
-  __index = function(t, k)
-    local module = "project-tools." .. k
+  __index = function(self, k)
+    local module = "project-tools."
+
+    -- if project config is already loaded
+    -- call functions for specific language
+    --
+    if (self._lang) then
+      module = module .. self._lang .. "."
+    end
+
+    module = module .. k
+
     local ok,val = pcall(require, module)
 
     if ok then
-      rawset(t, k, val)
+      rawset(self, k, val)
     else
       val = nil
     end
